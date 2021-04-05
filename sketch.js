320e446a5331e07b636213e91ff3b9a78ede41be
soovinchoi <soovinnnnnn@gmail.com>
2021-04-05 14:18:12 +0900

    tutorial

--- sketch.js
+++ sketch.js
@@ -1,118 +1,50 @@
-//the people you might have been passed and words
+let array = [];
 
-let names = [{
-  name: "akina",
-  //word: "hey"
-}, {
-  name: "zak",
-  //word: "love you"
-}, {
-  name: "june",
-  //word: "sweet"
-}, {
-  name: "sara",
-  //word: "my friend!"
-}, {
-  name: "cindia",
-  //word: "super!"
-}, {
-  name: "nick",
-  //word: "wonderful"
-}, {
-  name: "sierra",
-  //word: "I'm down"
-}, {
-  name: "greg",
-  //word: "you can do it"
-}, {
-  name: "cris",
-  //word: "wanna go get some coffee?"
-}, {
-  name: "marta",
-  //word: "bless you"
-}, {
-  name: "miguel",
-  //word: "chao"
-}];
-
-let words = [{
-  word: "hey"
-}, {
-  word: "love you"
-}, {
-  word: "sweet"
-}, {
-  word: "my friend!"
-}, {
-  word: "super!"
-}, {
-  word: "wonderful"
-}, {
-  word: "I'm down"
-}, {
-  word: "you can do it"
-}, {
-  word: "wanna go get some coffee?"
-}, {
-  word: "bless you"
-}, {
-  word: "chao"
-}];
-
-let randomIndex;
-let randomIndex2;
-// let inconsolata;
-// function preload() {
-//   inconsolata = loadFont('assets/inconsolata.otf');
-// }
 
 function setup() {
-  createCanvas(400, 400);
-  background(238,130,238);
+  createCanvas(600, 600);
+  background(167, 200, 242);
 
-  // console.log("initial array is");
-  // console.log(names);
+  strokeWeight(10);
+  //noFill();
 
-  // names.shift();
-  // //pop removes the last thing of the array
-  // console.log("array after shift");
-  // console.log(names);
-  //
-  // names.unshift("greg");
-  // console.log("array after unshift");
-  // console.log(names);
-  // //unshift takes the premeter
-  // //shift and pop: takes away the first one
-  //names.splice(index, number);
-  // names.splice(4, 1);
-  // console.log("array after splice");
-  // console.log(names);
-  //splice to remove on that perticuilar section
 
 }
 
 function draw() {
-  //text('click to find your destiny moment of bumping into someone', 45, 200);
+  //==false = !mouseIsPressed
 
+  if (mouseIsPressed) {
+    //line(mouseX, mouseY, pmouseX, pmouseY);
+    background(255);
+    array.push([mouseX, mouseY]);
+    //grey scale random
+    stroke(map(mouseX, 0, 600, 0, 255, true))
+    // line(width - mouseX, height - mouseY, width - pmouseX, height - pmouseY);
+    // line(mouseX, mouseY, pmouseX, pmouseY);
+  }
 }
 
-function mousePressed() {
-  background(random(240,150,238));
+function keyTyped() {
+  //  console.log('key ${key} is being typed')
+  console.log("key" + key + "is being typed")
 
-//names random
-  randomIndex = int(random(names.length));
-  randomIndex2 = int(random(words.length));
-  textStyle(NORMAL);
-  textFont('Orbitron');
-  textAlign(CENTER, CENTER);
-  text(names[randomIndex].name, 180, 150);
 
-//words random
-  textStyle(ITALIC);
-  textFont('Homemade Apple');
-  textAlign(CENTER, CENTER);
-  text(words[randomIndex2].word, 180, 170);
+  if (key === 's') {
+    saveCanvas('fileName', 'png');
+  } else if (key === 'd') {
+    background(167, 200, 242);
 
-  names.splice(randomIndex, 1);
+    beginShape();
+    for (let i = 0; i < array.length; i++) {
+      //line(array[i][0], array[i][1], array[i + 1][0], array[i + 1][1]);
+      curveVertex(array[i][0], array[i][1]);
+    }
+    endShape();
+
+
+  }
+
+  return false;
 
 }
